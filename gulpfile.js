785e7f1843e46f1a095b727cea0eba812057c696
RonnChyran <ronny6993@gmail.com>
2016-04-13 20:51:30 -0400

    gulpfile: Validate with standard style.

--- gulpfile.js
+++ gulpfile.js
@@ -2,63 +2,61 @@ var gulp = require('gulp')
 var yaml = require('gulp-yaml')
 var jsonConcat = require('gulp-json-concat')
 var merge = require('merge-stream')
-var jeditor = require("gulp-json-editor")
-var rename = require("gulp-rename")
+var jeditor = require('gulp-json-editor')
+var rename = require('gulp-rename')
 var jsonFormat = require('gulp-json-format')
 var tv4 = require('gulp-tv4')
 var through = require('through2')
 var packageData = require('./package.json')
 
+gulp.task('default', function () {
+  var controllerJson = gulp.src('./controller/*.yml')
+    .pipe(yaml({ space: 0 }))
+    .pipe(tv4('./schema/controller.schema.json'))
+    .pipe(through.obj(function (file, enc, callback) {
+      callback(null, file)
+      if (!file.tv4.valid) {
+        throw new Error(file.tv4.error + ' at ' + file.path)
+      }
+    }))
+    .pipe(jsonConcat('Controllers.json', function (data) {
+      return new Buffer(JSON.stringify(data))
+    }))
 
-gulp.task('default', function() {
-    var controllerJson = gulp.src('./controller/*.yml')
-        .pipe(yaml({ space: 0 }))
-        .pipe(tv4('./schema/controller.schema.json'))
-        .pipe(through.obj(function(file, enc, callback) {
-            callback(null, file);
-            if (!file.tv4.valid) {
-                throw new Error(file.tv4.error + ' at ' + file.path);
-            }
-        }))
-        .pipe(jsonConcat('Controllers.json', function(data) {
-            return new Buffer(JSON.stringify(data))
-        }))
+  var platformJson = gulp.src('./platform/*.yml')
+    .pipe(yaml({ space: 0 }))
+    .pipe(tv4('./schema/platform.schema.json'))
+    .pipe(through.obj(function (file, enc, callback) {
+      callback(null, file)
+      if (!file.tv4.valid) {
+        throw new Error(file.tv4.error + ' at ' + file.path)
+      }
+    }))
+    .pipe(jsonConcat('Platforms.json', function (data) {
+      return new Buffer(JSON.stringify(data))
+    }))
 
-    var platformJson = gulp.src('./platform/*.yml')
-        .pipe(yaml({ space: 0 }))
-        .pipe(tv4('./schema/platform.schema.json'))
-        .pipe(through.obj(function(file, enc, callback) {
-            callback(null, file);
-            if (!file.tv4.valid) {
-                throw new Error(file.tv4.error + ' at ' + file.path);
-            }
-        }))
-        .pipe(jsonConcat('Platforms.json', function(data) {
-            return new Buffer(JSON.stringify(data))
-        }))
-
-    merge(platformJson, controllerJson)
-        .pipe(jsonConcat('stone.json', function(data) {
-            return new Buffer(JSON.stringify(data))
-        }))
-        .pipe(jeditor({
-            'version': packageData.version
-        }))
-        .pipe(gulp.dest('./dist'))
-        .pipe(rename("stone.dist.json"))
-        .pipe(jsonFormat(0))
-        .pipe(gulp.dest('./dist'))
-
+  merge(platformJson, controllerJson)
+    .pipe(jsonConcat('stone.json', function (data) {
+      return new Buffer(JSON.stringify(data))
+    }))
+    .pipe(jeditor({
+      'version': packageData.version
+    }))
+    .pipe(gulp.dest('./dist'))
+    .pipe(rename('stone.dist.json'))
+    .pipe(jsonFormat(0))
+    .pipe(gulp.dest('./dist'))
 })
 
-gulp.task('format-platform', function() {
-    var platformSrc = gulp.src('./platform/*.yml')
-        .pipe(yaml({ space: 2, safe: true }))
-        .pipe(gulp.dest("./src/platform"))
+gulp.task('format-platform', function () {
+  gulp.src('./platform/*.yml')
+    .pipe(yaml({ space: 2, safe: true }))
+    .pipe(gulp.dest('./src/platform'))
 })
 
-gulp.task('format-controller', function() {
-    var controllerSrc = gulp.src('./controller/*.yml')
-        .pipe(yaml({ space: 2, safe: true }))
-        .pipe(gulp.dest("./src/controller"))
+gulp.task('format-controller', function () {
+  gulp.src('./controller/*.yml')
+    .pipe(yaml({ space: 2, safe: true }))
+    .pipe(gulp.dest('./src/controller'))
 })
